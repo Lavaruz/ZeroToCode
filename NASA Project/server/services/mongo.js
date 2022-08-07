dd26a3a125158202d1463153dab30880bcb174a0
Assami Muzaki <muzakiassami@gmail.com>
2022-08-07 22:15:40 +0700

    rotate database access

--- NASA Project/server/services/mongo.js
+++ NASA Project/server/services/mongo.js
@@ -2,8 +2,7 @@ const path = require('path')
 require('dotenv').config({path: path.resolve(__dirname, '../.env')})
 const mongoose = require('mongoose')
 
-DATABASE_KEY = process.env.NASA_DATABASE_KEY
-DATABASE_CONNECT = `mongodb+srv://Lavaruz:${DATABASE_KEY}@clusternasaproject.qykq0vb.mongodb.net/?retryWrites=true&w=majority`
+DATABASE_CONNECT = process.env.DATABASE_AUTH
 
 async function mongoConnect(){
     await mongoose.connect(DATABASE_CONNECT)
