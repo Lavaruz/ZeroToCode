9ab53b1c90473d30b55cbcaa5f6ae65755c8f004
Assami Muzaki <muzakiassami@gmail.com>
2022-08-09 22:07:00 +0700

    adding google oauth to app

--- Passport-Tutorial/OAuth2.0 Google Try/server.js
+++ Passport-Tutorial/OAuth2.0 Google Try/server.js
@@ -1,15 +1,61 @@
+require('dotenv').config()
+
 const express = require('express')
 const helmet = require('helmet')
 const https = require('https')
 const fs = require('fs')
 const path = require('path')
 
+// SECURITY
+const passport = require('passport')
+const StrategyOAuth = require('passport-google-oauth20').Strategy
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+}
+
+
+const strategyOption = {
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET,
+    callbackURL: "https://localhost:3000/auth/google/callback"
+}
+
+function verify(accessToken, refreshToken, profile, done){
+    console.log('Google Profile : ', profile);
+    done(null, profile)
+}
+
+passport.use(new StrategyOAuth(strategyOption, verify))
+
 const app = express()
 app.use(helmet())
 app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.get('/', (req, res) => {
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback', 
+    passport.authenticate('google', { failureRedirect: '/', session: false }),
+    function(req, res) {
+    // Successful authentication, redirect home.
+        res.redirect('/');
+});
+
+function checkLogin(req,res,next){
+    isLogin = true
+    if (!isLogin){
+        return res.status(401).json({
+            error: 'You have to login'
+        })
+    }
+    next()
+}
+
+app.get('/', checkLogin, (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'base.html'))
 })
 
