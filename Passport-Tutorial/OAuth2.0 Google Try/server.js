6f728f03aacfd172abfc3fb0f4c8255cfa8a284e
Assami Muzaki <muzakiassami@gmail.com>
2022-08-09 20:53:39 +0700

    do expreiment with static middleware

--- Passport-Tutorial/OAuth2.0 Google Try/server.js
+++ Passport-Tutorial/OAuth2.0 Google Try/server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const helmet = require('helmet')
 const https = require('https')
 const fs = require('fs')
+const path = require('path')
 
 const app = express()
 app.use(helmet())
+app.use(express.static(path.join(__dirname, 'public')))
+
+
 app.get('/', (req, res) => {
-    res.send('home page')
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 app.get('/secret', (req,res) =>{
