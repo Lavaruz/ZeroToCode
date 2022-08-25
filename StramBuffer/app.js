fbb3bc05ab420d7d083b234ca95174014159afec
Assami Muzaki <muzakiassami@gmail.com>
2022-08-25 20:47:38 +0700

    writeStream

--- StramBuffer/app.js
+++ StramBuffer/app.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
 const path = require('path')
 
+const writeStream = fs.createWriteStream(path.join(__dirname, 'newText.txt'))
 fs.createReadStream(path.join(__dirname, 'text.txt'))
     .on('data', (chunk)=>{
         console.log(chunk)
         console.log(Date.now());
+        writeStream.write(chunk, ()=> console.log('done'))
     })
